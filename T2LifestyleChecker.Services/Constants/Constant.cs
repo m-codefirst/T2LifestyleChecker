8cd2818d280fc254a069f84856b0a9291481a9d4
mansoornn <mansoornn@gmail.com>
2025-06-25 16:26:33 +0100

    Update Constant properties and added comments on frontend apiClient

--- T2LifestyleChecker.Services/Constants/Constant.cs
+++ T2LifestyleChecker.Services/Constants/Constant.cs
@@ -3,16 +3,7 @@
     public static class Constant
     {
         public const string ApiCallFailedWithStatusCodeMessage = "API call failed with status {StatusCode}";
-        public const string AgeGroups = "AgeGroups";
-        public const string Scoring = "Scoring";
-        public const string Messages = "Messages";
         public const string ScoreLow = "ScoreLow";
         public const string ScoreHigh = "ScoreHigh";
-
-        public const string Age16_21 = "16-21";
-        public const string Age22_40 = "22-40";
-        public const string Age41_65 = "41-65";
-        public const string Age64Plus = "64+";
-
     }
 }
--- T2LifestyleChecker/Client/src/api/apiClient.ts
+++ T2LifestyleChecker/Client/src/api/apiClient.ts
@@ -3,8 +3,9 @@ import axios from 'axios';
 // Create a pre-configured Axios instance for API calls
 const api = axios.create({
   // Base URL for all HTTP requests made using this instance
-  // This saves you from repeating the full URL every time you call api.get or api.post
-  baseURL: 'http://localhost:5206/api/LifestyleChecker',
+  // This saves you from repeating the full URL every time you call api.get or api.post  7032
+  // Test with two different ports 5206,7032
+  baseURL: 'https://localhost:7032/api/LifestyleChecker',
 });
 
 export default api;
